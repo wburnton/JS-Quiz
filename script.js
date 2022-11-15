84d5f9062a9df8adc4151a24a8db55164bf71109
Will Burnton <willburnton@Wills-MacBook-Air.local>
2022-11-15 16:58:44 -0600

    timer functions

--- script.js
+++ script.js
@@ -54,5 +54,39 @@ var questionSource = [
 
 ] 
 
+function startGame () {  
+  countdown(); 
+  timeInterval = setInterval(startTimer, 1000);
+    insertQuestion();
+}; 
 
-   
+
+
+function countdown() {
+  var timeLeft = 60;
+
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = '';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+      // Call the `displayMessage()` function
+      displayMessage();
+    }
+  }, 1000);
+
+
+}; 
+
+startButton.eventListener("click", startGame);
